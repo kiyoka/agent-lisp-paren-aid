73fe92763dcfddccecd85eb237c5a03974e9cea7
Kiyoka Nishiyama <kiyoka@sumibi.org>
2025-06-29 00:07:33 +0900

    added missing_paren-3.lisp

---
+++ tests/fixtures/missing_paren-3.lisp
@@ -0,0 +1,15 @@
+;
+(require 'url)
+(require 'url-http)
+
+(defun hello (roman arg-n)
+  (condition-case _err
+      (if (= "a" "b")
+	  (princ "true")
+	  (princ "false"))
+    ;; error path
+    (error
+     (printc "error!"))))
+  
+(defun nextfunc ()
+  (princ ""))
