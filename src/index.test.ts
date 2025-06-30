a1835ee957ea48279efafea070394facddf2a421
Kiyoka Nishiyama <kiyoka@sumibi.org>
2025-06-30 23:31:13 +0900

    Renamed *.lisp to *.el.

--- src/index.test.ts
+++ src/index.test.ts
@@ -8,52 +8,52 @@ describe('checkParenthesesLogic', () => {
     return fs.readFileSync(path.join(fixturesDir, fileName), 'utf8');
   };
 
-  test('should return "ok" for balanced parentheses (balanced-0.lisp)', () => {
-    const lispCode = loadFixture('balanced-0.lisp');
+  test('should return "ok" for balanced parentheses (balanced-0.el)', () => {
+    const lispCode = loadFixture('balanced-0.el');
     expect(checkParenthesesLogic(lispCode)).toBe('ok');
   });
 
-  test('should return "ok" for balanced parentheses (balanced-1.lisp)', () => {
-    const lispCode = loadFixture('balanced-1.lisp');
+  test('should return "ok" for balanced parentheses (balanced-1.el)', () => {
+    const lispCode = loadFixture('balanced-1.el');
     expect(checkParenthesesLogic(lispCode)).toBe('ok');
   });
 
-  test('should return an error for missing closing parentheses (missing_paren-0.lisp)', () => {
-    const filePath = path.join(fixturesDir, 'missing_paren-0.lisp');
+  test('should return an error for missing closing parentheses (missing_paren-0.el)', () => {
+    const filePath = path.join(fixturesDir, 'missing_paren-0.el');
     const lispCode = fs.readFileSync(filePath, 'utf8');
     const expectedError = `Error: line 2: Missing 2 closing parentheses.`;
     expect(checkParenthesesLogic(lispCode, filePath)).toBe(expectedError);
   });
 
-  test('should return an error for missing closing parentheses (missing_paren-1.lisp)', () => {
-    const filePath = path.join(fixturesDir, 'missing_paren-1.lisp');
+  test('should return an error for missing closing parentheses (missing_paren-1.el)', () => {
+    const filePath = path.join(fixturesDir, 'missing_paren-1.el');
     const lispCode = fs.readFileSync(filePath, 'utf8');
-    const expectedError = `Error: line 11: Missing 1 closing parentheses.`;
+    const expectedError = `Error: line 9: Missing 1 closing parentheses.`;
     expect(checkParenthesesLogic(lispCode, filePath)).toBe(expectedError);
   });
 
-  test('should return an error for missing closing parentheses (missing_paren-2.lisp)', () => {
-    const filePath = path.join(fixturesDir, 'missing_paren-2.lisp');
-    const lispCode = fs.readFileSync(filePath, 'utf8');
-    const expectedError = `Error: line 12: Missing 1 closing parentheses.`;
-    expect(checkParenthesesLogic(lispCode, filePath)).toBe(expectedError);
-  });
-
-  test('should return an error for missing closing parentheses (missing_paren-3.lisp)', () => {
-    const filePath = path.join(fixturesDir, 'missing_paren-3.lisp');
+  test('should return an error for missing closing parentheses (missing_paren-2.el)', () => {
+    const filePath = path.join(fixturesDir, 'missing_paren-2.el');
     const lispCode = fs.readFileSync(filePath, 'utf8');
     const expectedError = `Error: line 10: Missing 1 closing parentheses.`;
     expect(checkParenthesesLogic(lispCode, filePath)).toBe(expectedError);
   });
 
-  test('should return an error for extra closing parentheses (extra_paren-0.lisp)', () => {
-    const lispCode = loadFixture('extra_paren-0.lisp');
+  test('should return an error for missing closing parentheses (missing_paren-3.el)', () => {
+    const filePath = path.join(fixturesDir, 'missing_paren-3.el');
+    const lispCode = fs.readFileSync(filePath, 'utf8');
+    const expectedError = `Error: line 9: Missing 1 closing parentheses.`;
+    expect(checkParenthesesLogic(lispCode, filePath)).toBe(expectedError);
+  });
+
+  test('should return an error for extra closing parentheses (extra_paren-0.el)', () => {
+    const lispCode = loadFixture('extra_paren-0.el');
     const expectedError = 'Error: line 2: There are extra 1 closing parentheses.';
     expect(checkParenthesesLogic(lispCode)).toBe(expectedError);
   });
 
-  test('should return an error for extra closing parentheses (extra_paren-1.lisp)', () => {
-    const lispCode = loadFixture('extra_paren-1.lisp');
+  test('should return an error for extra closing parentheses (extra_paren-1.el)', () => {
+    const lispCode = loadFixture('extra_paren-1.el');
     const expectedError = 'Error: line 9: There are extra 1 closing parentheses.';
     expect(checkParenthesesLogic(lispCode)).toBe(expectedError);
   });
--- tests/fixtures/missing_paren-1.el
+++ tests/fixtures/missing_paren-1.el
@@ -10,4 +10,3 @@
 
 (defun nextfunc ()
   (princ ""))
-
